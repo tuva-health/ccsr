91f540dda3d38c6f6f9bff1adfb74d9ff2f485bc
cpashk <chasepas@gmail.com>
2023-03-13 19:46:37 -0400

    cleanup file, add vars to project

--- models/intermediate/ccsr__dx_wide_pivot.sql
+++
@@ -1,9 +0,0 @@
-with unique_ccsr_categories as (
-    
-    select distinct ccsr_category from {{ ref('ccsr__dx_vertical_pivot')}}
-
-)
--- jinja for loop to create a column for each row in CTE
-select distinct  
-*
-from unique_ccsr_categories
