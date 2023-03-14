09f0a9b0c0a752741d380861d511df9609dfca0f
cpashk <chasepas@gmail.com>
2023-03-14 11:25:14 -0400

    resolve primary output

--- models/ccsr__condition_ccsr_vertical.sql
+++ models/ccsr__condition_ccsr_vertical.sql
@@ -21,7 +21,7 @@ select
     dxccsr_vertical_codes.is_ip_default_category,
     dxccsr_vertical_codes.is_op_default_category,
     {{ var('dxccsr_version') }} as dxccsr_version
-from condition_records as aa
-left join dxccsr_vertical_codes as bb using(code)
+from condition_records 
+left join dxccsr_vertical_codes using(code)
 
     
--- models/ccsr__condition_default_category.sql
+++ models/ccsr__condition_default_category.sql
@@ -1,6 +1,8 @@
 select 
     claim_id,
+    ccsr_category,
     {{ var('dxccsr_version') }} as dxccsr_version
  from {{ref('ccsr__condition_ccsr_vertical')}}
  where 
     is_ip_default_category = true
+    and diagnosis_rank = 1
