454e19e6b2703d0985a800697594fe14623d06ad
cpashk <chasepas@gmail.com>
2023-03-14 14:43:05 -0400

    add models for procedures

---
+++ models/ccsr__wide_procedure_category.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+{% set category_query %}
+select distinct 
+    prccsr
+from {{ ref('dxccsr_v2023_1_cleaned_map') }}
+{% endset %}
+
+{% set categories = run_query(category_query) %}
+{% if execute %}
+{% set categories_list = categories.columns[0].values() %}
+{% endif %}
+
+select * from {{ var('procedure') }}
