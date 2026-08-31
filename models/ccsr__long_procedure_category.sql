d68574dcc0a1503be6afab709b9c32653232027e
Aaron Neiderhiser <anneiderhiser@gmail.com>
2026-08-30 21:58:31 -0600

    fix CCSR SQL portability on Fabric

--- models/ccsr__long_procedure_category.sql
+++ models/ccsr__long_procedure_category.sql
@@ -1,4 +1,4 @@
-with procedure as (
+with procedure_records as (
 
     select
           encounter_id
@@ -16,9 +16,9 @@ with procedure as (
 )
 
 select distinct
-    procedure.encounter_id,
-    procedure.person_id,
-    procedure.code,
+    procedure_records.encounter_id,
+    procedure_records.person_id,
+    procedure_records.code,
     ccsr__procedure_category_map.code_description,
     ccsr__procedure_category_map.ccsr_parent_category,
     ccsr__procedure_category_map.ccsr_category,
@@ -26,5 +26,6 @@ select distinct
     ccsr__procedure_category_map.clinical_domain,
     {{ var('prccsr_version') }} as prccsr_version,
     '{{ dbt_utils.pretty_time(format="%Y-%m-%d %H:%M:%S") }}' as _model_run_time
-from procedure
-left join ccsr__procedure_category_map using(code)
+from procedure_records
+left join ccsr__procedure_category_map
+    on procedure_records.code = ccsr__procedure_category_map.code
